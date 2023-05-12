5b113af1a49bcfd8d67aee2c6c398521b66e0491
Kal Zheng <kal.zheng@inspire-dt.com>
2023-05-12 16:05:40 +0800

    add confir for input bix

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -4,7 +4,14 @@ import PrimaryButton from "../components/PrimaryButton";
 function StartGameScreen() {
     return (
         <View style={styles.inputContainer}>
-            <TextInput style={styles.numberInput} maxLength={2}/>
+            <TextInput
+                style={styles.numberInput}
+                maxLength={2}
+                keyboardType="number-pad"
+                //not really needed but, meh
+                autoCapitalize="none"
+                autoCorrect={false}
+            />
             <PrimaryButton>Reset</PrimaryButton>
             <PrimaryButton>Confirm</PrimaryButton>
         </View>
