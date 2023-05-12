c2b6ace040cf71c89ea25e4d04fe37d4e7675516
Kal Zheng <kal.zheng@inspire-dt.com>
2023-05-12 15:56:58 +0800

    more change for start game screen, add shadow for page

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -16,8 +16,16 @@ export default StartGameScreen;
 const styles = StyleSheet.create({
     inputContainer: {
         // flex: 1,
-        padding: 16,
-        marginTop: 100,
         backgroundColor: '#72063c',
+        borderRadius: 8,
+        marginHorizontal: 24,
+        marginTop: 100,
+        padding: 16,
+        elevation: 4,//shadow for android
+        //for ios
+        shadowColor: 'black',
+        shadowOffset: { width: 0, height: 2 },
+        shadowRadius: 6,
+        shadowOpacity: 0.25,
     }
 });
