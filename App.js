523e756a73facd35c6782887f41334de743b4e18
Kal Zheng <kal.zheng@inspire-dt.com>
2023-05-17 11:35:51 +0800

    add status bar to change color of icon e.g battery, etc

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import { LinearGradient } from 'expo-linear-gradient';
 import { useFonts } from 'expo-font';//use specify font type
 // import AppLoading from 'expo-app-loading';// deprecated
 import * as SplashScreen from 'expo-splash-screen';
+import { StatusBar } from 'expo-status-bar';
 
 import StartGameScreen from './screens/StartGameScreen';
 import GameScreen from './screens/GameScreen';
@@ -66,23 +67,26 @@ export default function App() {
   }
 
   return (
-    <LinearGradient
-      colors={[Colors.primary700, Colors.accent500]}
-      style={styles.rootScreen}
-      onLayout={onLayoutRootView}
-    >
-      <ImageBackground
-        resizeMode='cover'
+    <>
+      <StatusBar style='light'/>
+      <LinearGradient
+        colors={[Colors.primary700, Colors.accent500]}
         style={styles.rootScreen}
-        imageStyle={styles.backgroundImage}
-        source={require('./assets/images/background.png')}
+        onLayout={onLayoutRootView}
       >
-        {/* <StartGameScreen /> */}
-        <SafeAreaView style={styles.rootScreen}>
-          {screen}
-        </SafeAreaView>
-      </ImageBackground>
-    </LinearGradient>
+        <ImageBackground
+          resizeMode='cover'
+          style={styles.rootScreen}
+          imageStyle={styles.backgroundImage}
+          source={require('./assets/images/background.png')}
+        >
+          {/* <StartGameScreen /> */}
+          <SafeAreaView style={styles.rootScreen}>
+            {screen}
+          </SafeAreaView>
+        </ImageBackground>
+      </LinearGradient>
+    </>
   );
 }
 
